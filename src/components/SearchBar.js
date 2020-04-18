f19864e9879b827e8f4bee1f8ab6f9b17082a06c
rafatmyo <rafatmyo@gmail.com>
2020-04-18 12:56:30 -0500

    Added: Controlled Elements

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    render() {
-        return (
-            <div className="ui segment">
-                <form className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input type="text"/>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  state = {term: ''}
+  
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({term: e.target.value.toLocaleUpperCase()})}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
